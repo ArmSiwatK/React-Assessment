6385d27ca9d5642aa69ce933a63552f8f92ab44c
ArmSiwatK <arm.siwatk@gmail.com>
2023-04-19 09:34:47 +0700

    Video fix

--- src/Owner/Bio.jsx
+++ src/Owner/Bio.jsx
@@ -1,8 +1,14 @@
+import YouTube from 'react-youtube';
+
 const Bio = () => {
     return (
         <div className="bio-container">
             <p className="paragraph-1">I am a junior software developer and English-Thai translator who recently completed Generation's  Bootcamp. My background in translation has honed my adaptability, which I believe to be my  strongest asset. I am confident in my ability to quickly learn and integrate new technologies into  my skill set, making me a valuable addition to any project. In my free time, I enjoy exploring my  interests in literature, video games, and music. I am also a hobbyist in 3D animation.</p>
-            <video className="bio-video" src="src/res/video.mp4" controls />
+            <YouTube
+                className="bio-video"
+                videoId="vGrtIMxlizY"
+                opts={{ height: '390', width: '640', playerVars: { autoplay: 1 } }}
+            />
             <p className="paragraph-2">45_Siwat (Arm/อาร์ม)</p>
         </div>
     )
